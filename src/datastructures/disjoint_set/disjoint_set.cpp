06a821833be4fefaa419f50a40cd4df45de7166e
Florian Schneider <floschne@mail.hs-ulm.de>
2017-02-07 16:23:51 +0100

    implemented disjoint_set::Node scaffolding with ctors

--- src/datastructures/disjoint_set/disjoint_set.cpp
+++ src/datastructures/disjoint_set/disjoint_set.cpp
@@ -1,5 +1,1 @@
-//
-// Created by p0w3r on 07.02.17.
-//
-
 #include "disjoint_set.h"
--- src/datastructures/disjoint_set/disjoint_set.h
+++ src/datastructures/disjoint_set/disjoint_set.h
@@ -1,10 +1,33 @@
 #ifndef LIBP0W3R_DISJOINT_SET_H
 #define LIBP0W3R_DISJOINT_SET_H
 
+#include <ostream>
 
-class disjoint_set {
+namespace libp0w3r {
 
-};
+    template <class T>
+    class disjoint_set {
+
+    public:
 
 
+        struct Node final{
+            T _data;
+            Node* _parent;
+            int _rank;
+
+            Node() : _data(), _parent(this), _rank(0) {}
+            Node(const T& data) : _data(data), _parent(this), _rank(0) {}
+            Node(const T&& data) : _data(data), _parent(this), _rank(0) {}
+            Node(const T& data, Node* p) : _data(data), _parent(p), _rank(0) {}
+            Node(const T&& data, Node* p) : _data(data), _parent(p), _rank(0) {}
+
+            Node(const Node& n) = delete;
+            Node(const Node&& n) = delete;
+            void operator=(const Node& n) = delete;
+            void operator=(const Node&& n) = delete;
+        };
+    };
+
+}
 #endif //LIBP0W3R_DISJOINT_SET_H
