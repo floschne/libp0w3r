b9002727283877e4b5343f7a19c9ca47ea2ca815
Florian Schneider <floschne@mail.hs-ulm.de>
2017-02-07 02:53:36 +0100

    initial disjoint_set commit

---
+++ src/datastructures/disjoint_set/disjoint_set.cpp
@@ -0,0 +1,5 @@
+//
+// Created by p0w3r on 07.02.17.
+//
+
+#include "disjoint_set.h"
---
+++ src/datastructures/disjoint_set/disjoint_set.h
@@ -0,0 +1,10 @@
+#ifndef LIBP0W3R_DISJOINT_SET_H
+#define LIBP0W3R_DISJOINT_SET_H
+
+
+class disjoint_set {
+
+};
+
+
+#endif //LIBP0W3R_DISJOINT_SET_H
