987af3dca0ab2bfa83f7733ba2dc7fdae57b3ad3
Florian Schneider <floschne@mail.hs-ulm.de>
2017-02-07 17:24:16 +0100

    implemented disjoint_set::Node TwoParameterCtor Test

--- test/datastructures/disjoint_set/NodeTest.h
+++ test/datastructures/disjoint_set/NodeTest.h
@@ -12,3 +12,19 @@ TEST(disjointSetTests, NodeTestStandardCtor) {
     ASSERT_EQ(n._data, 0);
     ASSERT_EQ(n._rank, 0);
 }
+
+TEST(disjointSetTests, NodeTestTwoParameterCtor) {
+
+    disjoint_set<int>::Node n1;
+
+    ASSERT_EQ(n1._parent, &n1);
+    ASSERT_EQ(n1._data, 0);
+    ASSERT_EQ(n1._rank, 0);
+
+    int n2_data{1337};
+    disjoint_set<int>::Node n2(n2_data, &n1);
+
+    ASSERT_EQ(n2._parent, &n1);
+    ASSERT_EQ(n2._data, n2_data);
+    ASSERT_EQ(n1._rank, 0);
+}
