c0641f9d7e10ea526f5f91b99fcde290faa1c624
Florian Schneider <floschne@mail.hs-ulm.de>
2017-02-09 11:26:06 +0100

    implemented ExceptionTests libp0w3rExceptionTest

---
+++ test/exceptions/ExceptionTest.h
@@ -0,0 +1,21 @@
+#ifndef LIBP0W3R_EXCEPTIONTEST_H
+#define LIBP0W3R_EXCEPTIONTEST_H
+
+#include <gtest/gtest.h>
+
+#include "../../src/exceptions/Libp0w3rException.h"
+
+using namespace libp0w3r;
+
+TEST(ExceptionTests, Libp0w3rExceptionTest) {
+
+    try {
+        throw Libp0w3rException();
+    } catch (Libp0w3rException& e) {
+        std::string msg{e.what()};
+
+        ASSERT_EQ(msg, "libp0w3r Exception thrown!\n");
+    }
+}
+
+#endif //LIBP0W3R_EXCEPTIONTEST_H
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "datastructures/disjoint_set/NodeTest.h"
+#include "exceptions/ExceptionTest.h"
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
