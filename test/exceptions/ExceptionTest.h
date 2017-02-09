7b178159a19cad53393e54cec8988c7fc78f96c3
Florian Schneider <floschne@mail.hs-ulm.de>
2017-02-09 11:30:26 +0100

    implemented ExceptionTests IllegalNullptrAssignmentExceptionTest

--- test/exceptions/ExceptionTest.h
+++ test/exceptions/ExceptionTest.h
@@ -18,4 +18,14 @@ TEST(ExceptionTests, Libp0w3rExceptionTest) {
     }
 }
 
+TEST(ExceptionTests, IllegalNullptrAssignmentExceptionTest) {
+    try {
+        throw IllegalNullptrAssignmentException("Test");
+    } catch (Libp0w3rException& e) {
+        std::string msg{e.what()};
+
+        ASSERT_EQ(msg, "libp0w3r Exception thrown!\n=> IllegalNullptrAssignmentException\nTest");
+    }
+}
+
 #endif //LIBP0W3R_EXCEPTIONTEST_H
